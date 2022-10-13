737de8f408b13ea8ba50c9be202795cb9bbd5d8b
Rupulsttiky27 <115666629+Rupulsttiky27@users.noreply.github.com>
2022-10-12 19:08:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const fs = require("fs")
 const { json } = require('express/lib/response')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3001
 const undici = require("undici")
 const funcaptcha = require("funcaptcha")
 
